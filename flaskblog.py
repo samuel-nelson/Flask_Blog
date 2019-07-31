5aa42f793899155126ee3f3c595a26b4b7506e00
Samuel Nelson <snelson54@cnm.edu>
2019-07-31 12:13:27 -0500

    Added routes for home and about

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'Samuel Nelson',
+        'title' : 'Blog Post 1',
+        'content' : 'first post content',
+        'date_posted' : '01-23-1987'
+    },
+    {
+        'author' : 'John Doe',
+        'title' : 'Blog Post 2',
+        'content' : 'second post content',
+        'date_posted' : '01-12-1387'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
